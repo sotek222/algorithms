5abae4f890024b678b61bde69ac4f7a9ad95f1a7
Matthew Masiello <sotek222@yahoo.com>
2020-03-04 10:38:16 -0500

    adds more tests

--- hacker_rank_problems/repeatedString/test.js
+++ hacker_rank_problems/repeatedString/test.js
@@ -3,3 +3,18 @@ const repeatedString = require('./index');
 test('repeatedString is a function', () =>   {
   expect(typeof repeatedString).toEqual('function');
 });
+
+describe("repeatedString returns the correct number of 'a'", () => {
+  test("repeatedString works with 'aba' & 10", () => {
+    expect(repeatedString('aba', 10)).toEqual(7);
+  });
+
+  test("repeatedString works with 'abcac' & 10", () => {
+    expect(repeatedString('abcac', 10)).toEqual(4);
+
+  }); 
+
+  test("repeatedString works with 'a' & 100000", () => {
+    expect(repeatedString('a', 100000)).toEqual(100000);
+  });
+});
