c64e33bfc6c77c18a85a3ba2ad868068f72d6607
Matthew Masiello <sotek222@yahoo.com>
2020-03-24 12:42:25 -0400

    adds initial problem statement and test

---
+++ hacker_rank_problems/minimumDistances/index.js
@@ -0,0 +1,22 @@
+// PROBLEM STATEMENT:
+// We define the distance between two array values as the 
+// number of indices between the two values.
+// Given an array (arr),find the minimum distance between 
+// any pair of equal elements in the array.
+// If no such value exists, print -1.
+
+// The minimumDistances function should return the minimum distance 
+// between any two matching elements.
+
+// minimumDistances has the following parameter(s):
+
+// arr: an array of integers
+
+// Examples: 
+// arr = [3,2,1,2,3]
+// minimumDistances(arr)
+// => 2 (the distance between 2 && 2)
+
+function minimumDistances(arr){};
+
+module.exports = minimumDistances;
---
+++ hacker_rank_problems/minimumDistances/test.js
@@ -0,0 +1,5 @@
+const minDistance = require('./index');
+
+test('minDistance is a function', () => {
+  expect(typeof minDistance).toEqual('function')
+});
